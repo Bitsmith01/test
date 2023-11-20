57868bc1664773a97b3cf502068a773fbfcbc2e5
Bitsmith01 <esperat.dimon@epitech.eu>
2023-11-20 16:49:57 +0100

    Project initialisation

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className='h-12 flex justify-between items-center p-5 bg-blue-700'>
+      ok
+    </nav>
+  )
+}
+
+export default Navbar
